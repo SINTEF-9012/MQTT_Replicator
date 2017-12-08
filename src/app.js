53ac4f37617ad40df001dbb84e301cadc2c84c97
Antoine Pultier <Antoine.Pultier@sintef.no>
2017-12-08 11:59:11 +0200

    can now subscribe to topics more intelligently

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import starter from './starter.js';
 import Concierge from './concierge.js';
 
-const { clients, mergeFrequencyHz } = starter();
+const { clients, mergeFrequencyHz, topics } = starter();
 
 const concierge = new Concierge(clients, (1.0 / mergeFrequencyHz) * 1000.0);
 
@@ -13,9 +13,21 @@ function broadcast(packet, retain = false, clientToIgnore = null) {
     });
 }
 
+
+
 clients.forEach((client) => {
     client.connect();
-    client.subscribe("#", 2);
+
+
+    topics.forEach((topic) => {
+        if ((topic.whitelist && !topic.whitelist.has(client.name)) ||
+            (topic.blacklist && topic.blacklist.has(client.name))) {
+            return;
+        }
+        client.subscribe(topic.topic, topic.qos);
+    });
+
+    //client.subscribe("#", 2);
     /*client.events.on('message', (packet) => {
         console.log(`Broadcasting from ${client.name}: ${packet.topic}/${packet.payload}`);
         broadcast(packet, client);
--- src/starter.js
+++ src/starter.js
@@ -1,30 +1,65 @@
 import Client from './client.js';
 
 // Each client is a process environment variable
-const client_env_match = /^MQTT_REPLICATOR_CLIENT_(.+)$/;
+const mqttrep_env_match = /^MQTTREP_(CLIENT|TOPIC)_(\w+)$/;
 
 export default function starter() {
 
     const clients = [];
+    const topics = [];
     for (let env in process.env) {
-        let m = env.match(client_env_match);
-        if (!m || env.endsWith('_CONFIG')) continue;
+        const m = env.match(mqttrep_env_match);
 
-        let name = m[1];
-        let path = process.env[env];
+        // Ignore environments variable which are not for us
+        // or configuration variables
+        if (!m) continue;
 
-        // Loading the configuration
-        let config = {};
-        let configPath = env + '_CONFIG';
-        if (process.env[configPath]) {
-            try {
-                config = JSON.parse(process.env[configPath]);
-            } catch (e) {
-                console.log(`Unable to parse the configuration ${configPath}: ${e.message}`);
-            }
+        const action = m[1];
+        const name = m[2];
+        const value = process.env[env];
+
+        switch (action) {
+            case 'CLIENT':
+                // Loading the configuration
+                let config = {};
+                let configPath = `MQTTREP_CONFIG_${name}`;
+                if (process.env[configPath]) {
+                    try {
+                        config = JSON.parse(process.env[configPath]);
+                    } catch (e) {
+                        console.log(`Unable to parse the configuration ${configPath}: ${e.message}`);
+                    }
+                }
+
+                clients.push(new Client(name, value, config));
+
+                break;
+            case 'TOPIC':
+                const topic = {
+                    name,
+                    topic: value,
+                    qos: parseInt(process.env[`MQTTREP_QOS_${name}`] || 1, 10),
+                    persistent: process.env[`MQTTREP_PERSISTENT_${name}`] === "true",
+                    ttl: parseInt(process.env[`MQTTREP_TTL_${name}`] || 1200, 10),
+                };
+
+                const whitelist = (process.env[`MQTTREP_WHITELIST_${name}`] || '')
+                    .split(',').map(s => s.trim()).filter(s => s.length);
+                const blacklist = (process.env[`MQTTREP_BLACKLIST_${name}`] || '')
+                    .split(',').map(s => s.trim()).filter(s => s.length);
+
+                if (whitelist.length) {
+                    topic.whitelist = new Set(whitelist);
+                }
+
+                if (blacklist.length) {
+                    topic.blacklist = new Set(blacklist);
+                }
+
+                topics.push(topic);
+                break;
         }
 
-        clients.push(new Client(name, path, config));
     }
     if (clients.length === 0) {
         console.log("No client has been configured.");
@@ -34,6 +69,7 @@ export default function starter() {
 
     return {
         clients,
-        mergeFrequencyHz
+        mergeFrequencyHz,
+        topics,
     };
 }
