9e0bb619a1f0aa1b6a741c8b75fc929e68964a09
Antoine Pultier <Antoine.Pultier@sintef.no>
2017-12-11 14:00:52 +0200

    Empty binary packets are equals to empty string packets

--- src/packet.js
+++ src/packet.js
@@ -7,15 +7,25 @@ export default class Packet {
                 a.topic === b.topic &&
                 (!checkQos || a.qos === b.qos) &&
                 (
+                    // Compare binary messages
                     (a.payload instanceof Buffer &&
                         b.payload instanceof Buffer &&
                         a.payload.length === b.payload.length &&
                         Buffer.compare(a.payload, b.payload) === 0
                     )
                     ||
+                    // Compare string messages
                     (a.payload instanceof String &&
                         b.payload instanceof String &&
                         a === b.payload
+                    )
+                    ||
+                    // Empty binary and empty string messages
+                    // are equals
+                    (
+                        a.payload !== undefined &&
+                        b.payload !== undefined &&
+                        a.payload.length === b.payload.length
                     )
                 ));
     }
