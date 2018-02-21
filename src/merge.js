d6737732054b43d5891a4d3e000d80f4c7b93357
Antoine Pultier <Antoine.Pultier@sintef.no>
2018-02-21 15:32:24 +0100

    msgpack and geobuf support

--- src/merge.js
+++ src/merge.js
@@ -1,6 +1,9 @@
 import log from './logger.js';
 import newMatcher from './matcher.js';
 import get from 'lodash.get';
+import msgpack from 'msgpack-lite';
+import geobuf from 'geobuf';
+import Pbf from 'pbf';
 
 function latestSelector(a, b) {
     return a.receivedTimestamp > b.receivedTimestamp ? a : b;
@@ -8,6 +11,19 @@ function latestSelector(a, b) {
 
 function biggestBinarySelector(a, b) {
     return Buffer.compare(a.payload, b.payload) > 0 ? a : b;
+}
+
+function biggestObjectSelector(path, a, b, objectA, objectB) {
+    const valueA = get(objectA, path, -Number.MAX_VALUE);
+    const valueB = get(objectB, path, -Number.MAX_VALUE);
+
+    if (valueA > valueB) {
+        return a;
+    } else if (valueA < valueB) {
+        return b;
+    }
+
+    return latestSelector(a, b);
 }
 
 function biggestJsonSelector(path, a, b) {
@@ -22,17 +38,39 @@ function biggestJsonSelector(path, a, b) {
         jsonB = JSON.parse(b.payload);
     } catch (e) { }
 
-    const valueA = get(jsonA, path, -Number.MAX_VALUE);
-    const valueB = get(jsonB, path, -Number.MAX_VALUE);
-
-    if (valueA > valueB) {
-        return a;
-    } else if (valueA < valueB) {
-        return b;
-    }
-
-    return latestSelector(a, b);
+    return biggestObjectSelector(path, a, b, jsonA, jsonB);
 }
+
+function biggestMsgpackSelector(path, a, b) {
+    let msgpackA = {};
+    let msgpackB = {};
+
+    try {
+        msgpackA = msgpack.decode(a.payload);
+    } catch (e) { }
+
+    try {
+        msgpackB = msgpack.decode(b.payload);
+    } catch (e) { }
+
+    return biggestObjectSelector(path, a, b, msgpackA, msgpackB);
+}
+
+function biggestGeobufSelector(path, a, b) {
+    let geobufA = {};
+    let geobufB = {};
+
+    try {
+        geobufA = geobuf.decode(new Pbf(a.payload));
+    } catch (e) { }
+
+    try {
+        geobufB = geobuf.decode(new Pbf(b.payload));
+    } catch (e) { }
+
+    return biggestObjectSelector(path, a, b, geobufA, geobufB);
+}
+
 
 function selectPacket(diff, selector) {
     // TODO empty payloads that are more recents than all other messages ?
@@ -76,6 +114,7 @@ export default class Merge {
         // We then check the values in this order, giving more importance to algorithms
         // tested first
         if (matchMap.has('jsonbiggest')) return this.jsonbiggest.bind(this, matchMap.get('jsonbiggest'));
+        if (matchMap.has('msgpackbiggest')) return this.msgpackbiggest.bind(this, matchMap.get('msgpackbiggest'));
         if (matchMap.has('geobufbiggest')) return this.geobufbiggest.bind(this, matchMap.get('geobufbiggest'));
         if (matchMap.has('biggest')) return this.biggest;
         if (matchMap.has('lastwin')) return this.lastwin;
@@ -100,8 +139,12 @@ export default class Merge {
         return selectPacket(diff, biggestJsonSelector.bind(null, settings));
     }
 
+    msgpackbiggest(settings, diff) {
+        return selectPacket(diff, biggestMsgpackSelector.bind(null, settings));
+    }
+
     geobufbiggest(settings, diff) {
-        return biggest(diff);
+        return selectPacket(diff, biggestGeobufSelector.bind(null, settings));
     }
 
     lastwin(diff) {
